05c10d850676dc2b9164273dfdbf6b8c18f7f32e
Md. Abdur Rofiq Islam Sawom <arsawom09@gmail.com>
2026-09-14 17:25:57 +0600

    Create manual-linux.sh

---
+++ manual-linux.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+# Script outline and installation for Linux kernel build and rootfs creation.
+# Author: Siddhant Jajoo.
+
+set -e
+set -u
+
+OUTDIR=/tmp/aeld
+KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_VERSION=v5.15.163
+BUSYBOX_VERSION=1.33.1
+FINDER_APP_PATH=$(realpath $(dirname $0))
+ARCH=arm64
+CROSS_COMPILE=aarch64-none-linux-gnu-
+
+if [ $# -lt 1 ]
+then
+	echo "Using default directory ${OUTDIR} for output"
+else
+	OUTDIR=$1
+	echo "Using passed directory ${OUTDIR} for output"
+fi
+
+mkdir -p ${OUTDIR}
+
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    #Clone only if the repository does not exist
+    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    git clone ${KERNEL_REPO} --depth 1 --branch ${KERNEL_VERSION}
+fi
+if [ ! -e "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" ]; then
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
+
+    # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+fi
+
+echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
+
+echo "Creating the staging directory for the root filesystem"
+cd "$OUTDIR"
+if [ -d "${OUTDIR}/rootfs" ]
+then
+echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+sudo rm -rf ${OUTDIR}/rootfs
+fi
+
+# TODO: Create necessary base directories
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sys tmp usr/bin usr/lib usr/sbin var sbin root mnt
+
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+git clone https://git.busybox.net/busybox
+cd busybox
+git checkout ${BUSYBOX_VERSION}
+# TODO:  Configure busybox
+make distclean
+make defconfig
+else
+cd busybox
+fi
+
+# TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+
+cd ${OUTDIR}/rootfs
+
+echo "Library dependencies"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+
+# TODO: Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+sudo cp -L ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/
+sudo cp -L ${SYSROOT}/lib64/libm.so.6 lib64/
+sudo cp -L ${SYSROOT}/lib64/libresolv.so.2 lib64/
+sudo cp -L ${SYSROOT}/lib64/libc.so.6 lib64/
+
+# TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+
+# TODO: Clean and build the writer utility
+cd ${FINDER_APP_PATH}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+
+# TODO: Copy the finder related scripts and executables to the /home directory
+# on the target rootfs
+cp writer ${OUTDIR}/rootfs/home/
+cp finder.sh ${OUTDIR}/rootfs/home/
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp conf/username.txt ${OUTDIR}/rootfs/home/conf/
+cp finder-test.sh ${OUTDIR}/rootfs/home/
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+
+# TODO: Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
+
+# TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio.gz
+cd ${OUTDIR}
+gzip -f initramfs.cpio.gz
